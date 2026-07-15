d7210406e12bcf773ee6a744547fbf12c5e6e3de
xsvmyx <mahisamy04@gmail.com>
2026-07-15 16:48:11 +0100

    refactor: added logger instead of a print

--- worker/config/connection.py
+++ worker/config/connection.py
@@ -2,6 +2,9 @@ import psycopg2
 from openai import OpenAI
 from config.settings import OPENROUTER_API_KEY ,DATABASE_URL , OPENROUTER_BASE_URL
 from functools import lru_cache
+import logging  
+
+logger = logging.getLogger(__name__)
 
 def connect():
     conn = psycopg2.connect(DATABASE_URL)
@@ -12,6 +15,9 @@ def connect():
 
 @lru_cache(maxsize=1)
 def get_openrouter_client(): 
+
+    logger.info("Creating OpenRouter client...")
+    
     return OpenAI(
         base_url=OPENROUTER_BASE_URL,
         api_key=OPENROUTER_API_KEY,
