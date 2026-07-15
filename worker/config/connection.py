1241ef5a0c04ec3b02504483c3a4173d2f3d65ae
xsvmyx <mahisamy04@gmail.com>
2026-07-15 17:05:20 +0100

    logger import

--- worker/config/connection.py
+++ worker/config/connection.py
@@ -1,10 +1,7 @@
 import psycopg2
 from openai import OpenAI
-from config.settings import OPENROUTER_API_KEY ,DATABASE_URL , OPENROUTER_BASE_URL
-from functools import lru_cache
-import logging  
-
-logger = logging.getLogger(__name__)
+from config.settings import OPENROUTER_API_KEY ,DATABASE_URL , OPENROUTER_BASE_URL , logger
+from functools import lru_cache  
 
 def connect():
     conn = psycopg2.connect(DATABASE_URL)
@@ -17,7 +14,7 @@ def connect():
 def get_openrouter_client(): 
 
     logger.info("Creating OpenRouter client...")
-    
+
     return OpenAI(
         base_url=OPENROUTER_BASE_URL,
         api_key=OPENROUTER_API_KEY,
