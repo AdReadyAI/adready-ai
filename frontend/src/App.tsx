11de23616828d0bca27003a0dee556fcee7da08d
Xingyan-Xie <125090785@link.cuhk.edu.cn>
2026-07-17 23:05:34 +0800

    App.test2.tsx
    App.tsx should be changed in order to register the new login page route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,13 @@ import AppLayout from './components/layout/AppLayout'
 import UploadPage from './pages/UploadPage'
 import LoadingPage from './pages/LoadingPage'
 import ResultPage from './pages/ResultPage'
+import { LoginPage } from './pages/LoginPage'
 
 function App() {
   return (
     <Routes>
+      <Route path="login" element={<LoginPage />} />
+
       <Route element={<AppLayout />}>
         <Route index element={<Navigate to="/upload" replace />} />
         <Route path="upload" element={<UploadPage />} />
