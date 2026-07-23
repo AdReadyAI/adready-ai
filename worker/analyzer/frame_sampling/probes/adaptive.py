21a9762cc62fa9c6d86900872c85f1f996a5df5f
Ben Kwan <benkwan26@gmail.com>
2026-07-23 12:36:09 -0400

    Implement AdaptiveSampler

--- worker/analyzer/frame_sampling/probes/adaptive.py
+++ worker/analyzer/frame_sampling/probes/adaptive.py
@@ -1,5 +1,16 @@
+from dataclasses import dataclass
+
 from analyzer.frame_sampling.base import Probe, ProbeResult, Stage, register_probe
 from analyzer.frame_sampling.context import FrameContext
+
+
+_DEFAULT_THRESHOLD = 0.5
+
+
+@dataclass
+class AdaptiveSamplerResult(ProbeResult):
+    keyframe_count: int
+    keyframe_indices: tuple[int, ...]
 
 
 @register_probe(Stage.SAMPLE)
@@ -12,8 +23,35 @@ class AdaptiveSampler(Probe):
 
     name = "adaptive"
 
+    def __init__(self) -> None:
+        self._budget = 0.0
+        self._keyframe_indices: list[int] = []
+        self._threshold = self._configured_threshold()
+
+    def _configured_threshold(self) -> float:
+        try:
+            from config.settings import ADAPTIVE_KEYFRAME_THRESHOLD
+        except KeyError:
+            return _DEFAULT_THRESHOLD
+        return ADAPTIVE_KEYFRAME_THRESHOLD
+
+    def _keep_keyframe(self, ctx: FrameContext) -> None:
+        ctx.keep(("keyframe",))
+        self._keyframe_indices.append(ctx.index)
+
     def process(self, ctx: FrameContext) -> None:
-        pass  # TODO: keyframe selection from content_val
+        if ctx.shot_boundary:
+            self._keep_keyframe(ctx)
+            self._budget = 0.0
+            return
+
+        self._budget += ctx.content_val
+        if self._budget >= self._threshold:
+            self._keep_keyframe(ctx)
+            self._budget = 0.0
 
     def finalize(self) -> ProbeResult:
-        return ProbeResult()
+        return AdaptiveSamplerResult(
+            keyframe_count=len(self._keyframe_indices),
+            keyframe_indices=tuple(self._keyframe_indices),
+        )
